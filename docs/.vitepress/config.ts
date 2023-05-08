2aa53c06745ca0df53986c6031580cf4dc9b9cd8
HADES <1005710179@qq.com>
2023-05-08 09:17:26 +0800

    update: 更新配置文件

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 import { head,nav,sidebar } from './configs'
 export default defineConfig({
   base: '/',
+  ignoreDeadLinks: true,
   title: "HADES",
   description: "HADES的成长之路，包含前端常用知识、源码阅读笔记、各种奇淫技巧、日常提效工具等",
   head,
