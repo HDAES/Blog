c1d49d4d65fa03bbe58c476f6a1214be7be63313
HDAES <1005710179@qq.com>
2024-02-23 23:55:15 +0800

    feat: 日常笔记

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,18 +1,18 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
-    { text: '导航', link: '/nav/' },
-    {
-        text: '前端物语',
-        items: [
-            { text: 'JavaScript', link: '/frontEnd-story/web/js-base' },
-            { text: 'TypeScript', link: '/frontEnd-story/ts/ts-decorator' },
-            { text: 'Threejs', link: '/frontEnd-story/auto/github-actions' },
-            { text: '自动化', link: '/frontEnd-story/webgl/threejs' },
-        ]
-    },
-    {
-        text: '日常笔记',
-        link: '/daily-notes/'
-    },
+  { text: '导航', link: '/nav/' },
+  {
+    text: '前端物语',
+    items: [
+      { text: 'JavaScript', link: '/frontEnd-story/web/js-base' },
+      { text: 'TypeScript', link: '/frontEnd-story/ts/ts-decorator' },
+      { text: 'Threejs', link: '/frontEnd-story/auto/github-actions' },
+      { text: '自动化', link: '/frontEnd-story/webgl/threejs' },
+    ],
+  },
+  {
+    text: '日常笔记',
+    items: [{ text: '旅游', link: '/daily-notes/travel/' }],
+  },
 ]
